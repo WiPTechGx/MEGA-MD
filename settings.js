f791c90794b9e74b795cfbbf80f50151914b15f4
pgwiz <pgwiz@users.noreply.github.com>
2026-08-09 17:19:04 +0300

    fix(mode): default commandMode to public and support MODE, WORK_TYPE and DISABLED_GROUPS environment variables
    Co-Authored-By: pgwiz <pgwiz@users.noreply.github.com>

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ const settings = {
   botOwner: 'pgwiz', // set your name
   ownerNumber: ['254789462334', '62561080893516'], // Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: process.env.MODE || process.env.WORK_TYPE || process.env.WORKTYPE || "public",
   maxStoreMessages: 20,
   tempCleanupInterval: 1 * 60 * 60 * 1000, // 1 hours
   storeWriteInterval: 10000,
